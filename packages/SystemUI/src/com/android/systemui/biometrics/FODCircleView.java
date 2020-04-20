48559837f647da16311ea41fb9337f205c5f5337
Kuba Wojciechowski <nullbytepl@gmail.com>
2020-04-20 14:55:50 +0000

    FODCircleView: dispatch onPress to HAL only after dimming is applied
    * Works better than hal 1.1 on laurel_sprout
    
    Change-Id: Ic980d50fd58a8afa91506c8d573a1c784db8df01

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -92,6 +92,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     private boolean mIsCircleShowing;
     private boolean mCanUnlockWithFp;
 
+    private float mCurrentDimAmount = 0.0f;
+
     private Handler mHandler;
 
     private Timer mBurnInProtectionTimer;
@@ -338,6 +340,16 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         updateCutoutFlags();
 
         Dependency.get(ConfigurationController.class).addCallback(this);
+
+        getViewTreeObserver().addOnGlobalLayoutListener(() -> {
+            float drawingDimAmount = mParams.dimAmount;
+            if (mCurrentDimAmount == 0.0f && drawingDimAmount > 0.0f) {
+                dispatchPress();
+                mCurrentDimAmount = drawingDimAmount;
+            } else if (mCurrentDimAmount > 0.0f && drawingDimAmount == 0.0f) {
+                mCurrentDimAmount = drawingDimAmount;
+            }
+        });
     }
 
     @Override
@@ -440,7 +452,6 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         setDim(true);
         updateAlpha();
-        dispatchPress();
 
         mPaintFingerprint.setColor(mColor);
 
